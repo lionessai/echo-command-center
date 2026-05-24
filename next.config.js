4d33cb6266787564a10977f462f74a986b968178
Dorothea Thomas <dorothea@lionessfinancialconsultants.com>
2026-05-24 17:18:41 +0000

    fix: remove webpack config incompatible with Next.js 16 Turbopack

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  experimental: {
-    serverComponentsExternalPackages: ['googleapis', '@notionhq/client', 'unpdf'],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-    }
-    return config;
-  },
+  serverExternalPackages: ['googleapis', '@notionhq/client', 'unpdf'],
 };
 module.exports = nextConfig;
